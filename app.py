3baee5c7a8457cac0dc420ab9f2c4b30d25ddc3e
eokaue88 <okauesilva0909@gmail.com>
2026-04-24 21:05:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ st.markdown("""
 
     /* 🔥 Corrige somente a logo no celular */
     [data-testid="stImage"] {
-        transform: translateX(100px);
+        transform: translateX(110px);
     }
 }
 
